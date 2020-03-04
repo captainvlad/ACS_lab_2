1c16eb16ecfba2fcf7b357cb30eb15efab3d0bdd
captainvlad <zadorozhnyy@ucu.edu.ua>
2020-03-04 20:09:00 +0200

    Some functions fixed

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 #include <cmath>
 
-//double function(double x_1, double x_2){
-////    Counts value of a function in a given coordinate
-//    double s_1, s_2;
-//    int m = 5;
-//    for (int i = 1; i <= m; i++){
-//        s_1 += i * cos( (i + 1) * x_1 + 1 );
-//        s_2 += i * cos( (i + 1) * x_2 + 1 );
-//    }
-//    return -s_1 * s_2;
-//}
-
-double function(double x_1, double y_1) {
-//     sample of a function
-    return ( pow(x_1, 2) * y_1 ) / (pow(x_1,4) + pow(x_1, y_1) );
+double function(double x_1, double x_2){
+//    Counts value of a function in a given coordinate
+    double s_1, s_2;
+    int m = 5;
+    for (int i = 1; i <= m; i++){
+        s_1 += i * cos( (i + 1) * x_1 + 1 );
+        s_2 += i * cos( (i + 1) * x_2 + 1 );
+    }
+    return -s_1 * s_2;
 }
+
+//double function(double x_1, double y_1) {
+////     sample of a function
+//    return ( x_1 * x_1 * y_1 ) / (x_1 * x_1 * x_1 * x_1 + y_1 * y_1 );
+//}
 
 double integral(double x_1, double x_2, double y_1, double y_2, double step){
 //    Integral calculation using Trapezoidal rule
@@ -25,16 +25,15 @@ double integral(double x_1, double x_2, double y_1, double y_2, double step){
 
         for (double j = y_2; j >= y_1; j -= step){
             result += function(x_2, j);
-
         }
     }
-    return result / 2 * pow(step,2);
+    return result * pow(step, 2);
 }
 
 
 int main() {
-    double step = (double) 1/2;
+    double step = (double) 1/100;
 
-    std:: cout << integral(-100, 100, -100, 100, step);
+    std:: cout << integral(-50, -2, 2, 100, step);
     return 0;
 }
