c7ee17a1b7b6ec5a5f4dfb15a72bea5659bee536
captainvlad <zadorozhnyy@ucu.edu.ua>
2020-03-04 18:45:16 +0200

    Changes occured

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -1,35 +1,41 @@
 #include <iostream>
 #include <cmath>
 
-double function(double x_1, double x_2){
-//    Counts value of a function in a given coordinate
-    double s_1, s_2;
-    int m = 5;
-    for (int i = 1; i <= m; i++){
-        s_1 += i * cos( (i + 1)*x_1 + 1 );
-        s_2 += i * cos( (i + 1)*x_2 + 1 );
-    }
-    return -s_1 * s_2;
+//double function(double x_1, double x_2){
+////    Counts value of a function in a given coordinate
+//    double s_1, s_2;
+//    int m = 5;
+//    for (int i = 1; i <= m; i++){
+//        s_1 += i * cos( (i + 1)*x_1 + 1 );
+//        s_2 += i * cos( (i + 1)*x_2 + 1 );
+//    }
+//    return -s_1 * s_2;
+//}
+
+double function(double x_1, double y_1) {
+//     sample of a function
+    return x_1 + y_1;
 }
 
-double integral(double step, double x_1, double x_2, double y_1, double y_2){
-//    counts integral for 2-dimensional function with given step and bounds
-    if (x_1 >= x_2 || y_1 >= y_2 || step <= 0){
-        std::cout << "Wrong input given";
-        return 0;
-    }
+double integral(double x_1, double x_2, double y_1, double y_2, double step){
+//    Integral calculation using Trapezoidal rule
+    double result = 0;
 
-    double i_value = function(x_1, y_1);
-    for (double x = x_1; x <= x_2; x += step){
-        for (double y = y_1; y <= y_2; y += step){
-            i_value += function(x, y);
+    for (x_2; x_2 >= x_1; x_2 -= step){
+
+        for (double j = y_2; j >= y_1; j -= step){
+
+            result += function(x_2, j) + function(x_2 - step, j - step);
+
         }
     }
-    return i_value;
-
+    return result / 2 * step;
 }
 
+
 int main() {
-    std::cout << integral(0.125, -100, 100, -100, 100);
+    double step = (double) 1/2000;
+
+    std:: cout << integral(0, 5, 0, 5, step);
     return 0;
 }
