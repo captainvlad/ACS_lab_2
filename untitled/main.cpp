ee8ffc143ca5cb72582cba756d68fe7826e40694
captainvlad <zadorozhnyy@ucu.edu.ua>
2020-03-04 22:23:44 +0200

    Some functions fixed

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -9,7 +9,7 @@ double function(double x_1, double x_2){
         s_1 += i * cos( (i + 1) * x_1 + 1 );
         s_2 += i * cos( (i + 1) * x_2 + 1 );
     }
-    return -s_1 * s_2;
+    return - s_1 * s_2;
 }
 
 //double function(double x_1, double y_1) {
@@ -17,12 +17,17 @@ double function(double x_1, double x_2){
 //    return ( x_1 * x_1 * y_1 ) / (x_1 * x_1 * x_1 * x_1 + y_1 * y_1 );
 //}
 
-double integral(double x_1, double x_2, double y_1, double y_2, double step){
+//double function(double x, double y) {
+//    //     sample of a function
+//    double a = 20, b = 0.2, c = 2 * M_PI;
+//    return -a * exp(-b * sqrt(0.5 * (x * x + y * y))) - exp(0.5 * (cos(c * x) + cos(c * y))) + a + exp(1);
+//}
+
+double integral(double x_1, double x_2, double y_1, double y_2, int steps){
 //    Integral calculation using Trapezoidal rule
     double result = 0;
-
+    double step = ( x_2 - x_1 ) * ( y_2 - y_1 ) / steps;
     for (x_2; x_2 >= x_1; x_2 -= step){
-
         for (double j = y_2; j >= y_1; j -= step){
             result += function(x_2, j);
         }
@@ -30,10 +35,25 @@ double integral(double x_1, double x_2, double y_1, double y_2, double step){
     return result * pow(step, 2);
 }
 
+int ma(){
+//    Function to find and return amount of squares for accurate integral result;
+//    Default accuracy = 0.1 %
+    int cur_partition = 1000;
+    double value_1, value_2, relevant_error;
+
+    do {
+       relevant_error = (value_1 - value_2) / value_1;
+       value_1 = integral(-100, -1, -100, -1, cur_partition);
+       value_2 = integral(-100, -1, -100, -1, cur_partition * 2);
+       cur_partition *= 2;
+    }while (std::abs(relevant_error) > 0.001);
+    std::cout << cur_partition;
+    std::cout << value_1;
+    return cur_partition;
+}
 
 int main() {
-    double step = (double) 1/100;
-
-    std:: cout << integral(-50, -2, 2, 100, step);
+    ma();
+//    std::cout << integral(-100, 100, -100, 100, 1600000);
     return 0;
 }
