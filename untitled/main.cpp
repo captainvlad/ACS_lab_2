0a009dbc8ad0731cd2f51476dd9db19160b6b9f5
captainvlad <zadorozhnyy@ucu.edu.ua>
2020-03-07 02:11:53 +0200

    Latest version

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -5,7 +5,7 @@ double function(double x_1, double x_2){
 //    Counts value of a function in a given coordinate
     double s_1 = 0;
     double s_2 = 0;
-    int m = 2;
+    int m = 5;
     for (int i = 1; i <= m; i++){
         s_1 += i * cos( (i + 1) * x_1 + 1 );
         s_2 += i * cos( (i + 1) * x_2 + 1 );
@@ -23,7 +23,9 @@ double integral(double x_1, double x_2, double y_1, double y_2, int steps){
 //    Integral calculation using Trapezoidal rule
     double result = 0;
     double step = ( x_2 - x_1 ) * ( y_2 - y_1 ) / steps;
-    for (x_2; x_2 >= x_1; x_2 -= step){
+
+    for (x_2 ;x_2 >= x_1; x_2 -= step){
+
         for (double j = y_2; j >= y_1; j -= step){
             result += function(x_2, j);
         }
@@ -37,31 +39,20 @@ int ma(){
     int cur_partition = 1000;
     double value_1, value_2, relevant_error;
 
+    value_1 = integral(-100, 100, -100, 100, cur_partition);
     do {
-       value_1 = integral(-100, 100, -100, 100, cur_partition);
        value_2 = integral(-100, 100, -100, 100, cur_partition * 2);
        cur_partition *= 2;
        relevant_error = (value_1 - value_2) / value_1;
-       std::cout << std::abs(relevant_error) << "\n";
-//        std::cout << cur_partition << "\n";
+       value_1 = value_2;
+       std::cout << "Rel error:" <<  std::abs(relevant_error) << "\n";
+       std::cout << "Integral value: " << value_2 << "\n";
+//       std::cout << cur_partition << "\n";
     }while (std::abs(relevant_error) > 0.001);
     return cur_partition;
 }
 
 int main() {
-//    int c = 100;
-//    int l, f;
-//    for (int i = 0; i < 100; i++){
-//        for (int k = 0; k < 100; k++){
-//            if (function(i, k) < c){
-//                c = function(i, k);
-//            }
-//        }
-//    }
-//    std::cout << c << "\n";
-//    std::cout << l << "\n";
-//    std::cout << f << "\n";
     ma();
-//    std::cout << "\n" << function(-1.425528, 0.799950) << "\n";
     return 0;
 }
